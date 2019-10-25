1cef62b6c22d843450aff93e141d0267143dd403
Jorge Alvarez <jorgealvarez@Jorges-MacBook-Pro.local>
2019-10-24 20:04:16 -0400

    styled movie list

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ document.addEventListener('DOMContentLoaded', function () {
         var movieHTML = movieArray.map(function (movie) {
 
             var movieElement = `
-            <div id="${movie.imdbID}" class="movie">
-                <img class="movie-image" src="${movie.Poster}" alt="" />
-                <h2 class="movie-title">${movie.Title}</h2>
-                <span class="movie-release">${movie.Year}</span>
-                <button class="add-movie-btn">Add</button>
+            <div id="${movie.imdbID}" class="movie card p-0 col-md-3">
+                <img class="movie-image card-img-top" src="${movie.Poster}" alt="${movie.Title}" />
+                <div class="card-body">
+                    <h4 class="movie-title card-title">${movie.Title}</h4>
+                    <span class="movie-release card-text">${movie.Year}</span>
+                    <button class="add-movie-btn btn btn-secondary align-right float-right">Add</button>
+                </div>
             </div>\n`
 
             return movieElement;
